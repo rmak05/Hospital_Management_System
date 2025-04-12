5c524147ad78f6355bf3ac3f8e9d69070c0af4d5
ishan kinger <kingerishan@gmail.com>
2025-04-12 23:12:22 +0530

    Trying git

--- Hospital_Management_System/enumScreenId.h
+++ Hospital_Management_System/enumScreenId.h
@@ -11,5 +11,20 @@ enum class ScreenId {
 	patient_update		 = 4,
 	patient_login		 = 5,
 	patient_home		 = 6,
-	update_login_patient = 7
+	update_login_patient = 7,
+	doctor_login         = 8,
+	doctor_home          = 9,
+	appointment          = 10,
+	patient_appointment  = 11,
+	presc_appointment    = 12,
+	tests_appointment    = 13,
+	admit_appointment    = 14,
+	patient_record       = 15,
+	patient_details_home = 16,
+	test_history         = 17,
+	appointment_history  = 18,
+	patient_details      = 19,
+	test_results         = 20,
+	presc_given          = 21,
+	my_information       = 22
 };
