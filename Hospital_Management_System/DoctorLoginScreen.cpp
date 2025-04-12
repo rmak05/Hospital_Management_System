f39efd42e2b8998efa3ee0e9b314101926c6e46e
Ravilisetty Makarandh <rmak1969@gmail.com>
2025-04-12 23:51:53 +0530

    once again experimenting with merge conflicts

--- Hospital_Management_System/DoctorLoginScreen.cpp
+++ Hospital_Management_System/DoctorLoginScreen.cpp
@@ -11,5 +11,5 @@ Doctor_Login_Screen::Doctor_Login_Screen(int w_width, int w_height) : Screen(Scr
 
 	add_entity(std::make_shared<Button>(std::string("Login"), 30u, OutlineThickness, sf::Vector2f(200.0f, 80.0f), sf::Vector2f(get_center_coord(get_center_coord(LEFT_MARGIN, (w_width - 2 * LEFT_MARGIN) * 1.0f, 1300.0f) + 150.0f, 700.0f, 200.0f), 700.0f), sf::Color::Black, lightYellow, sf::Color::Black, ScreenId::doctor_home));
 
-	add_entity(std::make_shared<ImageBox>(std::string("./Images/logiinPatient.png"), sf::Vector2f(get_center_coord(LEFT_MARGIN, (w_width - 2 * LEFT_MARGIN) * 1.0f, 1300.0f) + 900.0f, 400.0f), sf::Vector2f(0.2f, 0.2f)));
+	add_entity(std::make_shared<ImageBox>(std::string("./Images/loginPatient.png"), sf::Vector2f(get_center_coord(LEFT_MARGIN, (w_width - 2 * LEFT_MARGIN) * 1.0f, 1300.0f) + 900.0f, 400.0f), sf::Vector2f(0.2f, 0.2f)));
 }
